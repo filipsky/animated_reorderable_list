ffb8a0814adcff82a56bb0bcc58c7995a8252716
Honza Filipsky <honzaf@4max.com>
2026-03-11 20:06:31 +0100

    Fixed typo in calculateDiff method

--- lib/src/builder/reorderable_animated_list_base.dart
+++ lib/src/builder/reorderable_animated_list_base.dart
@@ -255,7 +255,7 @@ abstract class ReorderableAnimatedListBaseState<
               oldList.indexWhere((oldItem) => isSameItem(oldItem, newList[i]));
 
           if (oldIndex != -1) {
-            if (isSameItem(newList[oldIndex], oldList[i])) {
+            if (!isSameItem(newList[oldIndex], oldList[i])) {
               swappedPairs.add([i, oldIndex]);
             }
           }
